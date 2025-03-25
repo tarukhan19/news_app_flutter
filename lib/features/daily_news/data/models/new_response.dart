f0353600b031fc8fda59a41ecabf13addca595b0
taru <tarukhan19@gmail.com>
2025-03-25 21:39:10 +0530

    add comment

--- lib/features/daily_news/data/models/new_response.dart
+++ lib/features/daily_news/data/models/new_response.dart
@@ -9,9 +9,9 @@ class NewsResponseModel {
 
   factory NewsResponseModel.fromJson(Map<String, dynamic> json) {
     return NewsResponseModel(
-      articles: (json["articles"] as List)
-          .map((article) => ArticleModel.fromJson(article))
-          .toList(),
+      articles: (json["articles"] as List) // 1️⃣ Extracts "articles" as a List
+          .map((article) => ArticleModel.fromJson(article)) // 2️⃣ Converts each JSON article into an `ArticleModel` object
+          .toList(), // 3️⃣ Converts the mapped results back into a List
     );
   }
 }
